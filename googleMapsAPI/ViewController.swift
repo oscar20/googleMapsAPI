3559e0c78f6e005d925bb90a3adb6969a175ec2f
oscar20 <oscar_almazan@outlook.com>
2018-05-25 18:46:13 -0500

    Cargando mapa basico con marcador

--- googleMapsAPI/ViewController.swift
+++ googleMapsAPI/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func loadView() {
+        
+        
+        //Este codigo es para marcar un lugar en especifico en el map 
+        /*
+        let camera = GMSCameraPosition.camera(withLatitude: 19.333236, longitude: -99.192253, zoom: 16)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 19.333236, longitude: -99.192253)
+        marker.title = "Ciudad Universitaria"
+        marker.snippet = "Estadio Olimpico"
+        marker.map = mapView
+        */
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
